c8bee69084ec31a878f7ba967b2853c041288320
Santosh Arron <73644573+santosharron@users.noreply.github.com>
2024-02-03 19:01:20 +0530

    Update hardhat.config.js

--- Contract/hardhat.config.js
+++ Contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/GxA0-iF_T4fQIm1DJX1KVrjBoYcjp_aE",
       accounts: [
-        "a5bf39eeec1576d909cc7c740ad843eed99d38287c67e849f850e24101912c28",
+        "YOUR_PRIVATE_KEY",
       ],
     },
   },
